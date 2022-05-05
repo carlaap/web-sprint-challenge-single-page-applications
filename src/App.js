43f620845611e0fc97efc4d0c57c6797ff52bb20
Byron Francisco Portillo <byronportillo@Byrons-MacBook-Pro.local>
2022-05-04 20:20:15 -0700

    finoshed sprint

--- src/App.js
+++ src/App.js
@@ -1,2 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Form from "./Components/Form";
+import Home from "./Components/Home";
 
+const App = () => {
+    return (
+        <div className="header">
+            <h1>Let's Build a Pie!</h1>
+            <nav>
+                <Link to="/"><button id="home-button">Home</button></Link>
+                <Link to="/pizza"><button id="order-pizza">Order Pizza</button></Link>
+            </nav>
+            <Switch>
+                <Route path="/pizza" component={Form} />
+                <Route path="/" component={Home} />
+            </Switch>
+            </div>
+    );
+};
+
+export default App;
 
---
+++ src/Components/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <h1>Home</h1>
+    return <h1>Click on the form to start building your pie!</h1>
+}
+export default Home;
